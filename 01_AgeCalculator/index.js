177314edc311a7d9cdcd85bd363e652543c142f2
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-05-19 13:12:01 +0200

    Adding logic to the project 1

--- 01_AgeCalculator/index.js
+++ 01_AgeCalculator/index.js
@@ -1,41 +1,27 @@
-// Catching the input date
+const btnEl = document.getElementById('btn');
+const birthdayEl = document.getElementById('birthday');
 
-document.getElementById('dateInput').addEventListener('change', function () {
-	var input = this.value;
-	var dateEntered = new Date(input);
-	console.log(input); //e.g. 2015-11-13
-	console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+const calculateAge = () => {
+	console.log('clicked');
+	const birthdayValue = birthdayEl.value;
 
-	return (yourDateOfBirth = input);
+	if (birthdayValue == '') {
+		alert('Please select the date');
+	} else {
+		const age = getAge(birthdayValue);
+	}
+};
+
+const getAge = (birthdayValue) => {
+	const currentDate = new Date();
+	const birthdayDate = new Date(birthdayValue);
+
+	const yearOfBirth = birthdayDate.getFullYear();
+	const yourAge = currentDate.getFullYear() - yearOfBirth;
+
+	console.log(yourAge);
+};
+
+btnEl.addEventListener('click', () => {
+	calculateAge();
 });
-
-// Catching the today date
-
-let today = new Date().toISOString().slice(0, 10);
-
-console.log(today);
-
-// Counting the difference between the current date and the current date
-
-function dateDiffInDays(today, yourDateOfBirth) {
-	const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-	// Discard the time and time-zone information.
-
-	const utc1 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
-	const utc2 = Date.UTC(
-		yourDateOfBirth.getFullYear(),
-		yourDateOfBirth.getMonth(),
-		yourDateOfBirth.getDate()
-	);
-
-	console.log(Math.floor((utc2 - utc1) / _MS_PER_DAY));
-}
-
-// Displaying the difference
-
-dateDiffInDays();
-
-// If there is no input date, don't display the paragraph
-
-console.log('hello');
