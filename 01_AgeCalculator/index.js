5b52fd56ed0c08c31c4979b40e5bc66abe75d018
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-05-19 13:15:10 +0200

    Finishing the project 1

--- 01_AgeCalculator/index.js
+++ 01_AgeCalculator/index.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById('btn');
 const birthdayEl = document.getElementById('birthday');
+const resultEl = document.getElementById('result');
 
 const calculateAge = () => {
 	console.log('clicked');
@@ -9,6 +10,7 @@ const calculateAge = () => {
 		alert('Please select the date');
 	} else {
 		const age = getAge(birthdayValue);
+		resultEl.innerHTML = `Your age is ${age}`;
 	}
 };
 
@@ -20,6 +22,7 @@ const getAge = (birthdayValue) => {
 	const yourAge = currentDate.getFullYear() - yearOfBirth;
 
 	console.log(yourAge);
+	return yourAge;
 };
 
 btnEl.addEventListener('click', () => {
