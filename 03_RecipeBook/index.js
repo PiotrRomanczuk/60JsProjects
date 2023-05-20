3783d691fc3bedd09608913b1bf8c7642f34d48d
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-05-20 10:02:27 +0200

    Fetching the data from API

---
+++ 03_RecipeBook/index.js
@@ -0,0 +1,21 @@
+const API_KEY = '3d31646544a94c96bf5df39f618e1bfc';
+
+const getRecipes = async () => {
+	const API_URL = 'https://api.spoonacular.com/';
+	// const options = { 'Content-Type': application/json };
+
+	const response = await fetch(
+		`${API_URL}/recipes/random?number=10&apiKey=${API_KEY}`
+	);
+
+	const data = await response.json();
+
+	return data.recipes;
+};
+
+const init = async () => {
+	const recipes = await getRecipes();
+	console.log(recipes);
+};
+
+init();
