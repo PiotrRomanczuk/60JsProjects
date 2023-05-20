f09b67b9345a94c7553557cfbaa7c922aa262950
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-05-20 13:57:22 +0200

    Finishing project 3

--- 03_RecipeBook/index.js
+++ 03_RecipeBook/index.js
@@ -3,6 +3,8 @@ const API_KEY = '3d31646544a94c96bf5df39f618e1bfc';
 
 // console.log(dotenv);
 
+const recipeListEl = document.getElementById('recipe-list');
+
 const getRecipes = async () => {
 	const API_URL = 'https://api.spoonacular.com/';
 	// const options = { 'Content-Type': application/json };
@@ -12,13 +14,53 @@ const getRecipes = async () => {
 	);
 
 	const data = await response.json();
-
 	return data.recipes;
+};
+
+const displayRecipe = (recipes) => {
+	const recipeListEl = document.getElementById('recipe-list');
+
+	if (!recipeListEl) {
+		console.error('recipe-list element not found');
+		return;
+	} else {
+		console.log('The recipe list has been found');
+	}
+
+	recipeListEl.innerHTML = '';
+
+	if (!Array.isArray(recipes) || recipes.length === 0) {
+		console.warn('No recipes found');
+		return;
+	}
+
+	recipes.forEach((recipe) => {
+		const recipeItemEl = document.createElement('li');
+		recipeItemEl.classList.add('recipe-item');
+
+		const recipeImageEl = document.createElement('img');
+		recipeImageEl.src = recipe.image;
+		recipeImageEl.alt = 'recipe image';
+
+		const recipeTitleEl = document.createElement('h2');
+		recipeTitleEl.innerText = recipe.title;
+
+		const recipeIngredientsEl = document.createElement('p');
+		recipeIngredientsEl.innerHTML = `<strong>Ingredients:</strong>${recipe.extendedIngredients
+			.map((ingredient) => ingredient.original)
+			.join(', ')}`;
+
+		recipeItemEl.appendChild(recipeImageEl);
+		recipeItemEl.appendChild(recipeTitleEl);
+		recipeItemEl.appendChild(recipeIngredientsEl);
+		recipeListEl.appendChild(recipeItemEl);
+	});
 };
 
 const init = async () => {
 	const recipes = await getRecipes();
 	console.log(recipes);
+	displayRecipe(recipes);
 };
 
 init();
